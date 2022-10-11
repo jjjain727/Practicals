d92bc6822d306e870e69cc87f39552aebc777c66
jjjain727 <115546329+jjjain727@users.noreply.github.com>
2022-10-11 16:17:01 +0530

    Add files via upload

---
+++ CalculatorApp/src/java/CalculatorServlet.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jjjai
+ */
+@WebServlet(urlPatterns = {"/CalculatorServlet"})
+public class CalculatorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+             String s1 = request.getParameter("t1");
+            int num1 = Integer.parseInt(s1);
+            
+            String s2 = request.getParameter("t2");
+            int num2 = Integer.parseInt(s2);
+            
+            String op = request.getParameter("r1");
+            if(op.equals("Add")) {
+                int sum = num1 + num2;
+                out.println("Addition: " + sum);
+            }
+            else if(op.equals("Subtract")) {
+                int diff = num1 - num2;
+                out.println("Differene: " + diff);
+            }
+            else if(op.equals("Multiply")) {
+                int pro = num1 * num2;
+                out.println("Product: " + pro);
+            }
+            else if(op.equals("Divide")) {
+                double quo = num1 / num2;
+                out.println("Division: " + quo);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
